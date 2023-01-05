f0d0f3cc032c7a73d46e1c76d21f5f5c428a3027
cewarman <70783929+cewarman@users.noreply.github.com>
2023-01-05 18:39:19 +0800

    Update reqtest.py

--- reqtest.py
+++ reqtest.py
@@ -9,31 +9,35 @@ concatenated_lines = ""
 lines=f.readlines()
 for line in lines:
 	concatenated_lines = concatenated_lines + line
-concatenated_lines = concatenated_lines +"\n       "
+concatenated_lines = concatenated_lines +"\n"
+#print(concatenated_lines)
 
-loginURL = 'http://120.126.151.132/cgi-bin/RBTN.cgi'
+loginURL = 'http://120.126.151.132/'
 
 headers = {'User-Agent': 'ntpu-rbtn-user',}
-formdata = {'myTag':concatenated_lines}
+formdata = {'raw_text':concatenated_lines}
 session = requests.Session()
  
 response=session.post(loginURL ,data = formdata)
- 
+
 #goto_URL = 'http://120.126.151.132/cgi-bin/RBTN.cgi'
 #response = session.get(goto_URL, headers = headers)
  
 response.encoding="utf-8"
 
-key_start_line="</textarea>                    <textarea rows=\"9\" name=\"myTag\" cols=\"58\">"
-key_end_line="</textarea>                    <br>"
+key_start_line="<textarea rows=\"14\" cols=\"50\">"
+key_end_line="</textarea>"
 
 #print (response.text)
 flag=0
 lines=response.text.splitlines()
 for i in range(len(lines)):
-	if(lines[i]==key_end_line):
+	now=lines[i].strip()
+	if(now==key_end_line):
 		break
-	if(flag==1 and lines[i + 1] != key_end_line):
+	if(flag==1 and (lines[i]!='' or lines[i+1]!=key_end_line)):
 		print(lines[i])
-	if(lines[i]==key_start_line):
+	if(now[:30]==key_start_line[:30]):
+		print(now[30:])
 		flag=1
+	
